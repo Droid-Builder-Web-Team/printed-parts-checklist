e9a2cea85137d1dd2c6d2d580cd1773571b6b5a9
Rob Howdle <robhowdle94@gmail.com>
2022-05-01 15:11:46 +0100

    Set Up DataTables in the views

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Admin Dashboard') }}
+      {{ __('Users List') }}
     </h2>
   </x-slot>
 
@@ -9,8 +9,8 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
-          <a href="{{ route('admin-users') }}">Users List</a>
-          <a href="{{ route('admin-droids') }}">Droids List</a>
+          {!! $dataTable->table() !!}
+          {!! $dataTable->scripts() !!}
         </div>
       </div>
     </div>
