50a32231bd8f1bf5d8d9bb0b89e9dde828921f0a
Rob Howdle <robhowdle94@gmail.com>
2022-05-01 15:10:46 +0100

    Created Droid/User DataTable Files and Populated

---
+++ app/DataTables/UserDataTable.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\User;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Services\DataTable;
+
+class UserDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('delete', '')
+            ->editColumn(
+                'delete',
+                function ($row) {
+                    $crudRoutePart = "user";
+                    $parts = array('delete');
+                    return view('components.datatablesActions', compact('row', 'crudRoutePart', 'parts'));
+                }
+            )
+            // Make the name column clickable to view the profile of that user & mailto email - RH
+            ->editColumn('name', '<a href="#">{{$name}}</a>')
+            ->editColumn('email', '<a href="mailto:{{$email}}">{{$email}}</a>')
+            ->rawColumns(['name', 'email', 'delete']);
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->setTableId('user-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('Bfrtip')
+            ->lengthMenu([15, 25, 50])
+            ->orderBy(0)
+            ->buttons(
+                Button::make('create'),
+                Button::make('export'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('name'),
+            Column::make('email'),
+            Column::make('email_verified_at'),
+            Column::computed('delete')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'User_' . date('YmdHis');
+    }
+}
