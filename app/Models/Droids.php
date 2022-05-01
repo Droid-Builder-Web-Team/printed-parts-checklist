bb2154860907ef20d4e129ae92cea31c974902e5
Rob Howdle <robhowdle94@gmail.com>
2022-05-01 10:41:57 +0100

    Created Droids Model & Controller

---
+++ app/Models/Droids.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Droids extends Model
+{
+    use HasFactory;
+}
