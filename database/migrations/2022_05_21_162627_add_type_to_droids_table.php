290c9c9d9032d7a9b705f556bc761dadbec45869
Robert Howdle <79516357+RobertHowdle@users.noreply.github.com>
2023-03-13 17:19:59 +0000

    Seeding Droid Data

--- database/migrations/2022_05_21_162627_add_type_to_droids_table.php
+++ database/migrations/2022_05_21_162627_add_type_to_droids_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('droids', function (Blueprint $table) {
-            $table->string('type')->after('description');
+            $table->unsignedBigInteger('type_id')->after('description');
             $table->string('tags')->nullable()->change();
+
+            // Foreign Key
+            $table->foreign('type_id')->references('id')->on('droid_types');
         });
     }
 
@@ -27,7 +29,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('droids', function (Blueprint $table) {
-            $table->dropColumn('type');
+            $table->dropColumn('type_id');
         });
     }
 };
